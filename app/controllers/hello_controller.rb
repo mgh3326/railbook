a7eb156f9292c1f079746721dcc87ffa78f868fb
kwanghyunMoon <mgh3326@naver.com>
2019-12-12 21:49:55 +0900

    update 2.4 모델 기본

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -6,4 +6,16 @@ class HelloController < ApplicationController
   def view
     @msg = 'Hello World..!'
   end
+
+  def list
+    @books = Book.all
+  end
+
+  def app_var
+    render plain: MY_APP['logo']['source']
+  end
+
+  def validate_each(record, attribute, value)
+    #TODO: 무언가를 구현해주세요.
+  end
 end
